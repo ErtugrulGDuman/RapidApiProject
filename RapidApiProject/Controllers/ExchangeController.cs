25a5f00c3285afd92107265d8ed4079472fe8bdb
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-05-09 22:05:57 +0300

    Project Created...

--- RapidApiProject/Controllers/ExchangeController.cs
+++ RapidApiProject/Controllers/ExchangeController.cs
@@ -4,7 +4,7 @@ using RapidApiProject.Models;
 
 namespace RapidApiProject.Controllers
 {
-    public class ImdbSeriesController : Controller
+    public class ExchangeController : Controller
     {
         public async Task<IActionResult> Index()
         {
@@ -12,18 +12,18 @@ namespace RapidApiProject.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&language=en&to_symbol=TRY"),
                 Headers =
     {
         { "x-rapidapi-key", "ec8301ee9emsh2badfd42cf7a397p11c50bjsn18b7907cc04e" },
-        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
     },
             };
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ImdbSeriesViewModel>>(body);
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
                 return View(values);
             }
         }
